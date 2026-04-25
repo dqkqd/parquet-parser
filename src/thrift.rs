a94b31849f85dc8e96bac07f94111a8c7747df71
Khanh Duong <dqkqdlot@gmail.com>
2026-04-25 09:44:45 +0900

    `read_thrift_metadata` returns the number of byte read

--- src/thrift.rs
+++ src/thrift.rs
@@ -4,13 +4,14 @@ use anyhow::Result;
 use bytes::Bytes;
 use thrift::protocol::{TCompactInputProtocol, TSerializable};
 
-pub fn read_thrift_metadata<T: TSerializable>(data: &mut Bytes) -> Result<T> {
+/// Wrapper for reading parquet thrift metadata, returning the parsed data and the remaining Bytes.
+pub fn read_thrift_metadata<T: TSerializable>(data: Bytes) -> Result<(T, Bytes)> {
     let mut cursor = Cursor::new(data.as_ref());
     let decoded = {
         let mut protocol = TCompactInputProtocol::new(&mut cursor);
         T::read_from_in_protocol(&mut protocol)?
     };
-    *data = data.slice(cursor.position() as usize..);
+    let remaining = data.slice(cursor.position() as usize..);
 
-    Ok(decoded)
+    Ok((decoded, remaining))
 }
