68e5b1782044ad437eb4726fe7cef1f9f1c0d80b
Khanh Duong <dqkqdlot@gmail.com>
2026-04-25 15:59:59 +0900

    columns, reduce implementation effort

--- src/column.rs
+++ src/column.rs
@@ -14,12 +14,25 @@ fn column_name(column_metadata: &ColumnMetaData) -> String {
 /// TODO: docs
 #[allow(unused)]
 fn column_from_scalars(scalars: Vec<Scalar>, column_name: &str) -> Result<Column> {
-    todo!()
+    let values: Vec<AnyValue<'_>> = scalars
+        .into_iter()
+        .map(|scalar| scalar.into_value())
+        .collect();
+    let series = Series::from_any_values(column_name.into(), &values, true)?;
+    let column = Column::from(series);
+    Ok(column)
 }
 
 /// Read a [`Column`] from a parquet data.
 /// TODO: docs
 #[allow(unused_variables)]
 pub fn read_column(data: Bytes, column_chunk: &ColumnChunk) -> Result<Column> {
-    todo!()
+    let column_metadata = column_chunk
+        .meta_data
+        .as_ref()
+        .expect("read_column: missing column metadata");
+    // You should:
+    // - Get all the pages using `read_column_data_pages`
+    // - Decode all the pages using appropriate decoder into `scalars`
+    column_from_scalars(vec![], &column_name(column_metadata))
 }
