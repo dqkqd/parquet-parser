f2e6f9bb160dde5e997e458f3b0770c5ee570529
Khanh Duong <dqkqdlot@gmail.com>
2026-04-26 12:14:00 +0900

    solution step07-row-group

--- src/row_group.rs
+++ src/row_group.rs
@@ -2,15 +2,23 @@ use anyhow::Result;
 use bytes::Bytes;
 use polars::prelude::*;
 
-use crate::format::{FileMetaData, RowGroup};
+use crate::{
+    column::read_column,
+    format::{FileMetaData, RowGroup},
+};
 
 /// Read a row group into [`DataFrame`].
 ///
 /// A row group contains multiple column chunks.
 /// This function reads all the column chunks into a single [`DataFrame`].
-#[allow(unused_variables)]
 pub fn read_row_group(data: Bytes, row_group: &RowGroup) -> Result<DataFrame> {
-    todo!("step07: implement read row group")
+    let mut columns = Vec::with_capacity(row_group.columns.len());
+    for column_chunk in &row_group.columns {
+        let column = read_column(data.clone(), column_chunk)?;
+        columns.push(column);
+    }
+    let df = DataFrame::new_infer_height(columns)?;
+    Ok(df)
 }
 
 /// Read row groups into [`DataFrame`].
@@ -18,7 +26,19 @@ pub fn read_row_group(data: Bytes, row_group: &RowGroup) -> Result<DataFrame> {
 /// A file contains multiple row groups.
 /// This function reads all the row groups, and
 /// concatenate all the returned [`DataFrame`]s into a single [`DataFrame`].
-#[allow(unused_variables)]
 pub fn read_row_groups(data: Bytes, file_metadata: &FileMetaData) -> Result<DataFrame> {
-    todo!("step07: implement read row groups")
+    let mut dfs = Vec::with_capacity(file_metadata.row_groups.len());
+    for row_group in &file_metadata.row_groups {
+        let df = read_row_group(data.clone(), row_group)?;
+        dfs.push(df.lazy());
+    }
+    let df = concat(
+        dfs,
+        UnionArgs {
+            strict: true,
+            ..Default::default()
+        },
+    )?
+    .collect()?;
+    Ok(df)
 }
