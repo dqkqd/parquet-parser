92165fdf594fab97445103b376787d236feb35f3
Khanh Duong <dqkqdlot@gmail.com>
2026-04-26 20:13:36 +0900

    change signature bit packed decoder

--- src/decoder/bit_packed.rs
+++ src/decoder/bit_packed.rs
@@ -4,10 +4,14 @@ use polars::prelude::*;
 
 use crate::format::Type;
 
+/// Decode bit packed encoding data.
+///
+/// TODO: diagram
+/// [parquet encoding spec](https://parquet.apache.org/docs/file-format/data-pages/encodings/#RLE)
 /// TODO: docs. Only support boolean and u32
 #[allow(unused)]
 pub fn bit_packed_decode(
-    encoded_data: &mut Bytes,
+    mut encoded_data: Bytes,
     parquet_type: Type,
     bit_width: u8,
     num_values: usize,
