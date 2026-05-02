4a9eb7bf6ed7d9da2d2fd293e5d74b70d3121551
Khanh Duong <dqkqdlot@gmail.com>
2026-05-02 15:38:40 +0900

    remove parquet type in dictionary decode

--- src/decoder/dictionary.rs
+++ src/decoder/dictionary.rs
@@ -2,13 +2,10 @@ use anyhow::Result;
 use bytes::Bytes;
 use polars::prelude::*;
 
+#[allow(unused)] // avoid using different `Type` when implementing solution
 use crate::format::Type;
 
 #[allow(unused_variables)]
-pub fn dictionary_decode(
-    encoded_data: Bytes,
-    parquet_type: Type,
-    num_values: usize,
-) -> Result<Vec<Scalar>> {
+pub fn dictionary_decode(encoded_data: Bytes, num_values: usize) -> Result<Vec<Scalar>> {
     todo!()
 }
