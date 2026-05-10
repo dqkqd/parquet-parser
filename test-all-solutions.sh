182e6566674e892013c7f9bd61945021954640d3
Khanh Duong <dqkqdlot@gmail.com>
2026-05-10 09:20:13 +0900

    remove unneeded files

--- test-all-solutions.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-jj log -r 'description(glob:"solution step*")' --no-graph --reversed -T 'change_id ++ "|" ++ description.first_line() ++ "\n"' \
-| while IFS='|' read -r id desc; do
-    echo "=== Testing: $desc ==="
-    jj new "$id"
-    just lint || exit 1
-    cargo nextest run || exit 1
-done
